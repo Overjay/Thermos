d0662bd2bd9d7bcea084bd365cad6ab3e83758bb
engin <engin@steklo.local>
2020-11-25 17:07:09 +0200

    Initial commit

---
+++ src/com/overjay/MatLayer.java
@@ -0,0 +1,19 @@
+package com.overjay;
+
+
+public class MatLayer {
+    float thickness; //unit - 'meters'
+    float density; //unit - 'kg/m3'
+    float thermCond; //unit - 'W/m*K'
+    float thermResist; //unit - 'm2*K/W'
+    float steamPerm; //unit - mu - 'mg/(m*hour*Pa)
+    float steamResist; //unit - Re - '(m2*hour*Pa)/mg'
+
+    float layerThickness; //thickness of a layer that is to be created
+    int posNumber; //position in the order of addition from inside to outside
+    
+
+
+
+
+}
